0fd00af5d0f7691c74711a1ed6e0cec4a5d334bd
Javier Granados <jgranadosflores@gmail.com>
2019-11-13 17:05:20 +0100

    Revert "Test styled-components"
    This reverts commit be5dfe4649371e5f214329bc106f5ac2378886df.

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,31 +1,5 @@
-import styled from 'styled-components';
-
-const MyButton = styled.button`
-  background: ${props => props.background || '#ff6600'};
-  div {
-    font-size: 20px;
-  }
-`;
-
-const BlueButton = styled(MyButton)`
-  background: #1700e6;
-`;
-
 const Home = () => {
-  return (
-    <>
-      <p>You're in home page</p>
-      <MyButton>
-        <div>Default button</div>
-      </MyButton>
-      <MyButton background="#ffc600">
-        <div>Yellow button</div>
-      </MyButton>
-      <BlueButton>
-        <div>Blue button</div>
-      </BlueButton>
-    </>
-  );
+  return <p>You're in home page</p>;
 };
 
 export default Home;
