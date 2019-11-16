63d1c472ba8353e6e69242ffd6286984324fea6c
Javier Granados <jgranadosflores@gmail.com>
2019-11-16 02:34:57 +0100

    Apollo Boost migration to Apollo Client

--- sick-fits/frontend/utils/withData.js
+++ sick-fits/frontend/utils/withData.js
@@ -1,18 +1,35 @@
 import withApollo from 'next-with-apollo';
-import ApolloClient from 'apollo-boost';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
 import { endpoint } from '../config';
 
 function createClient({ headers }) {
   return new ApolloClient({
-    uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
-    request: operation => {
-      operation.setContext({
-        fetchOptions: {
-          credentials: 'include',
+    link: ApolloLink.from([
+      onError(({ graphQLErrors, networkError }) => {
+        if (graphQLErrors)
+          graphQLErrors.forEach(({ message, locations, path }) =>
+            console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
+          );
+        if (networkError) console.log(`[Network error]: ${networkError}`);
+      }),
+      new HttpLink({
+        uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
+        request: operation => {
+          operation.setContext({
+            fetchOptions: {
+              credentials: 'include',
+            },
+            headers,
+          });
         },
-        headers,
-      });
-    },
+      }),
+    ]),
+    cache: new InMemoryCache(),
+    connectToDevTools: process.env.NODE_ENV === 'development',
   });
 }
 
