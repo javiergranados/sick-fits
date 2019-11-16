0a778c61550700c401f1a95a7c0b93134966d181
Javier Granados <jgranadosflores@gmail.com>
2019-11-16 04:09:39 +0100

    Modify styles

--- sick-fits/frontend/components/styles/Item.js
+++ sick-fits/frontend/components/styles/Item.js
@@ -36,4 +36,35 @@ const Item = styled.div`
   }
 `;
 
-export { Item };
+const Title = styled.h3`
+  margin: 0 1rem;
+  text-align: center;
+  transform: skew(-5deg) rotate(-1deg);
+  margin-top: -3rem;
+  text-shadow: 2px 2px 0 rgba(0, 0, 0, 0.1);
+  a {
+    background: ${({ theme }) => theme.red};
+    display: inline;
+    line-height: 1.3;
+    font-size: 4rem;
+    text-align: center;
+    color: white;
+    padding: 0 1rem;
+  }
+`;
+
+const PriceTag = styled.span`
+  background: ${({ theme }) => theme.red};
+  transform: rotate(3deg);
+  color: white;
+  font-weight: 600;
+  padding: 5px;
+  line-height: 1;
+  font-size: 3rem;
+  display: inline-block;
+  position: absolute;
+  top: -3px;
+  right: -3px;
+`;
+
+export { Item, Title, PriceTag };
