0b1288c825b157568d70f90af4edd0e57d7bae53
Javier Granados <jgranadosflores@gmail.com>
2019-11-17 14:33:00 +0100

    Prisma deploy

--- backend/src/db.js
+++ backend/src/db.js
@@ -3,7 +3,7 @@
 const { Prisma } = require('prisma-binding');
 
 const db = new Prisma({
-  typeDefs: 'src/generated/prisma.graphql',
+  typeDefs: 'prisma/generated/prisma.graphql',
   endpoint: process.env.PRISMA_ENDPOINT,
   secret: process.env.PRISMA_SECRET,
   debug: false,
