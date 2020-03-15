eaef20f2c0648e90ad7b348d1b1d02094c157caf
jvgranados <jgranadosflores@gmail.com>
2020-03-16 00:17:45 +0100

    Implement some styles for pagination

--- frontend/components/Pagination.js
+++ frontend/components/Pagination.js
@@ -1,5 +1,6 @@
 import { useQuery } from '@apollo/react-hooks';
 import Head from 'next/head';
+import Link from 'next/link';
 import { PAGINATION } from '../graphql/query';
 import Error from './Error';
 import { perPage } from '../config';
@@ -21,9 +22,30 @@ const Pagination = ({ page }) => {
           Sick fits! | Page {page} of {pages}
         </title>
       </Head>
+      <Link
+        href={{
+          pathname: 'items',
+          query: { page: page - 1 },
+        }}
+      >
+        <a className="prev" aria-disabled={page <= 1}>
+          ⬅ Prev
+        </a>
+      </Link>
       <p>
         Page {page} of {pages}
       </p>
+      <p>{count} Items Total</p>
+      <Link
+        href={{
+          pathname: 'items',
+          query: { page: page + 1 },
+        }}
+      >
+        <a className="next" aria-disabled={page >= pages}>
+          Next ➡
+        </a>
+      </Link>
     </S.Pagination>
   );
 };
