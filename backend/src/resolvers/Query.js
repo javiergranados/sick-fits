88682e212ff265e1623c0ad427fcdc89f435c7b3
jvgranados <jgranadosflores@gmail.com>
2020-03-31 04:00:58 +0200

    Add new query on backend

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -8,6 +8,18 @@ const Query = {
   items: forwardTo('db'),
   item: forwardTo('db'),
   itemsConnection: forwardTo('db'),
+  me(parent, args, ctx, info) {
+    // check if there is a current user id
+    if (!ctx.request.userId) {
+      return null;
+    }
+    return ctx.db.query.user(
+      {
+        where: { id: ctx.request.userId },
+      },
+      info
+    );
+  },
 };
 
 module.exports = Query;
