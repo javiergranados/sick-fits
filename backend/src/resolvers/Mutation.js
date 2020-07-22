97dfbf53cce3c74348e48b23621f1543892fdd06
jvgranados <jgranadosflores@gmail.com>
2020-07-22 23:38:13 +0200

    Implement email submission

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { randomBytes } = require('crypto');
 const { promisify } = require('util');
+const { transport, makeANiceEmail } = require('../../mail');
 
 const Mutations = {
   async createItem(parent, args, ctx, info) {
@@ -32,9 +33,11 @@ const Mutations = {
   async deleteItem(parent, args, ctx, info) {
     const where = { id: args.id };
     // 1 - find the item
+
     // const item = await ctx.db.query.item({ where }, info);
     // 2 - Check if they own that item, or have the permissions
     // TODO
+
     // 3 - Delete it
     return ctx.db.mutation.deleteItem({ where }, info);
   },
@@ -46,18 +49,22 @@ const Mutations = {
     data.permissions = {
       set: ['USER'],
     };
+
     // create the user in the database
     const user = await ctx.db.mutation.createUser({
       data,
       info,
     });
+
     // create the JTW token for the user
     const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+
     // set the jwt as a cookie on the response
     ctx.response.cookie('token', token, {
       httpOnly: true,
       maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
     });
+
     // finally we return the user to the browser
     return user;
   },
@@ -72,18 +79,22 @@ const Mutations = {
     if (!user) {
       throw new Error(`No such user found for the email ${args.emai}`);
     }
+
     // 2- check if their password is correct
     const valid = await bcrypt.compare(args.password, user.password);
     if (!valid) {
       throw new Error('Invalid Password!');
     }
+
     // 3- Generate the JWT token
     const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+
     // 4- Set the cookie with the token
     ctx.response.cookie('token', token, {
       httpOnly: true,
       maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
     });
+
     // 5- Return the user
     return user;
   },
@@ -97,6 +108,7 @@ const Mutations = {
     if (!user) {
       throw new Error(`No such user found for the email ${args.emai}`);
     }
+
     // 2. Set a reset token and expirty on that user
     const randomBytesPromiseified = promisify(randomBytes);
     const resetToken = (await randomBytesPromiseified(20)).toString('hex');
@@ -105,15 +117,30 @@ const Mutations = {
       where: { email: args.email },
       data: { resetToken, resetTokenExpiry },
     });
-    return { message: 'Thanks!' };
+
     // 3. Email them that reset token
+    await transport.sendMail({
+      from: process.env.MAIL_FROM,
+      to: user.email,
+      subject: 'Your Password Reset Token',
+      html: makeANiceEmail(
+        `Your Password Reset Token is here!
+        \n\n
+        <a href="${process.env.FRONTEND_URL}/reset?resetToken=${resetToken}">Click Here to Reset</a>`
+      ),
+    });
+
+    // 4 Return the message
+    return { message: 'Thanks!' };
   },
   async resetPassword(parent, args, ctx) {
     // 1. Check if the passwords match
     if (args.password !== args.confirmPassword) {
       throw new Error("Your passwords don't match!");
     }
+
     // 2. Check if its a legit reset token
+
     // 3. Check if its expired
     const [user] = await ctx.db.query.users({
       where: {
@@ -124,8 +151,10 @@ const Mutations = {
     if (!user) {
       throw new Error('This token is either invalid or expired!');
     }
+
     // 4. Hash their new password
     const password = await bcrypt.hash(args.password, 10);
+
     // 5. Save the new passowrd to the user and remove old resetToken fields
     const updatedUser = await ctx.db.mutation.updateUser({
       where: { email: user.email },
@@ -135,13 +164,16 @@ const Mutations = {
         resetTokenExpiry: null,
       },
     });
+
     // 6. Generate JWT
     const token = jwt.sign({ userId: updatedUser.id }, process.env.APP_SECRET);
+
     // 7. Set the JWT cookie
     ctx.response.cookie('token', token, {
       httpOnly: true,
       maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
     });
+
     // 8 . Return the new user
     return updatedUser;
   },
