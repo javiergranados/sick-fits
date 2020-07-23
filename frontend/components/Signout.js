f55dd6b3cbf1a871c453bc86ebbae7b2f3e76f7a
jvgranados <jgranadosflores@gmail.com>
2020-07-23 15:26:08 +0200

    Change url after sign out

--- frontend/components/Signout.js
+++ frontend/components/Signout.js
@@ -1,12 +1,23 @@
+import { useRouter } from 'next/router';
 import { useMutation } from '@apollo/react-hooks';
 import { CURRENT_USER } from '../graphql/query';
 import { SIGN_OUT } from '../graphql/mutation';
 
 export default () => {
+  const router = useRouter();
   const [signOut] = useMutation(SIGN_OUT, { refetchQueries: [{ query: CURRENT_USER }] });
 
+  const handleClick = async () => {
+    try {
+      await signOut();
+      router.push('/');
+    } catch (err) {
+      // do nothing
+    }
+  };
+
   return (
-    <button type="button" onClick={signOut}>
+    <button type="button" onClick={handleClick}>
       Sign out
     </button>
   );
