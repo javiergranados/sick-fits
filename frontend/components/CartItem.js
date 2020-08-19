76f95152980017de6a686943e31e6c54e76a8852
jvgranados <jgranadosflores@gmail.com>
2020-08-19 17:22:54 +0200

    Dealing with deleted items

--- frontend/components/CartItem.js
+++ frontend/components/CartItem.js
@@ -4,20 +4,29 @@ import formatMoney from '../utils/formatMoney';
 import RemoveFromCart from './RemoveFromCart';
 import * as S from './styles/CartItem';
 
-const CartItem = ({ cartItem: { id, item, quantity } }) => {
+const CartItem = ({ cartItem }) => {
+  if (!cartItem.item) {
+    return (
+      <S.CartItem>
+        <p>This Item has been removed</p>
+        <RemoveFromCart id={cartItem.id} />
+      </S.CartItem>
+    );
+  }
+
   return (
     <S.CartItem>
-      <img width="100" src={item.image} alt={item.title} />
+      <img width="100" src={cartItem.item.image} alt={cartItem.item.title} />
       <div className="cart-item-details">
-        <h3>{item.title}</h3>
+        <h3>{cartItem.item.title}</h3>
         <p>
-          {formatMoney(item.price * quantity)}&nbsp;→&nbsp;
+          {formatMoney(cartItem.item.price * cartItem.quantity)}&nbsp;→&nbsp;
           <em>
-            {quantity} &times; {formatMoney(item.price)} each
+            {cartItem.quantity} &times; {formatMoney(cartItem.item.price)} each
           </em>
         </p>
       </div>
-      <RemoveFromCart id={id} />
+      <RemoveFromCart id={cartItem.id} />
     </S.CartItem>
   );
 };
