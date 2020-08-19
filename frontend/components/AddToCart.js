3171f9c44658e0ecbe310968d2b354ee4cda0b7d
jvgranados <jgranadosflores@gmail.com>
2020-08-19 14:15:22 +0200

    Implement refetch query

--- frontend/components/AddToCart.js
+++ frontend/components/AddToCart.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { useMutation } from '@apollo/react-hooks';
 import { ADD_TO_CART } from '../graphql/mutation';
+import { CURRENT_USER } from '../graphql/query';
 
 const AddToCart = ({ id }) => {
-  const [addToCart] = useMutation(ADD_TO_CART);
+  const [addToCart] = useMutation(ADD_TO_CART, {
+    refetchQueries: [{ query: CURRENT_USER }],
+  });
 
-  const handleClick = async () => {
+  const handleClick = () => {
     try {
-      const { data } = await addToCart({ variables: { id } });
-      console.log('data', data);
+      addToCart({ variables: { id } });
     } catch (err) {
       // do nothing
     }
--- frontend/graphql/query.js
+++ frontend/graphql/query.js
@@ -43,6 +43,10 @@ const CURRENT_USER = gql`
       name
       email
       permissions
+      cart {
+        id
+        quantity
+      }
     }
   }
 `;
