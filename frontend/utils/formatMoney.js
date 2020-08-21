6aad3f7f61562fcec7cf96b391bbd21fc177d79a
jvgranados <jgranadosflores@gmail.com>
2020-08-21 13:10:15 +0200

    Revert "Change currency from USD to EUR"
    This reverts commit c14a1478e1d01f2ee820b822016ab3906cc34d66.

--- frontend/utils/formatMoney.js
+++ frontend/utils/formatMoney.js
@@ -1,12 +1,11 @@
 export default function(amount) {
   const options = {
     style: 'currency',
-    currency: 'EUR',
+    currency: 'USD',
     minimumFractionDigits: 2,
   };
   // if its a whole, dollar amount, leave off the .00
   if (amount % 100 === 0) options.minimumFractionDigits = 0;
-  const formatter = new Intl.NumberFormat('it', options);
+  const formatter = new Intl.NumberFormat('en-US', options);
   return formatter.format(amount / 100);
 }
-// number.toLocaleString('it', { style: 'currency', currency: 'EUR' }) : '0,00 €';
