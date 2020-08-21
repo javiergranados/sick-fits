f55c3773bb8c3b38730cd10cfde1a1da5bf78394
jvgranados <jgranadosflores@gmail.com>
2020-08-21 17:32:26 +0200

    Create 'order' query on server side

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -35,6 +35,31 @@ const Query = {
     // If they dos, query all the users
     return ctx.db.query.users({}, info);
   },
+
+  async order(parent, args, ctx, info) {
+    // 1. Make sure they are logged in
+    if (!ctx.request.userId) {
+      throw new Error('You must be logged in to do that!');
+    }
+
+    // 2. Query the current order
+    const order = await ctx.db.query.order(
+      {
+        where: { id: args.id },
+      },
+      info
+    );
+
+    // 3. Check if they have the permissions to see this order
+    const ownsOrder = order.user.id === ctx.request.userId;
+    const hasPermissionToSeeOrder = ctx.request.user.permissions.includes('ADMIN');
+    if (!ownsOrder || !hasPermissionToSeeOrder) {
+      throw new Error('You cant see this buddd');
+    }
+
+    // 4. Return the order
+    return order;
+  },
 };
 
 module.exports = Query;
