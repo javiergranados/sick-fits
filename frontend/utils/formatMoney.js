c14a1478e1d01f2ee820b822016ab3906cc34d66
jvgranados <jgranadosflores@gmail.com>
2020-08-21 10:33:42 +0200

    Change currency from USD to EUR

--- frontend/utils/formatMoney.js
+++ frontend/utils/formatMoney.js
@@ -1,11 +1,12 @@
 export default function(amount) {
   const options = {
     style: 'currency',
-    currency: 'USD',
+    currency: 'EUR',
     minimumFractionDigits: 2,
   };
   // if its a whole, dollar amount, leave off the .00
   if (amount % 100 === 0) options.minimumFractionDigits = 0;
-  const formatter = new Intl.NumberFormat('en-US', options);
+  const formatter = new Intl.NumberFormat('it', options);
   return formatter.format(amount / 100);
 }
+// number.toLocaleString('it', { style: 'currency', currency: 'EUR' }) : '0,00 €';
