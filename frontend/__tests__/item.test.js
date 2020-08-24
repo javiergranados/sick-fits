5db6b6644b8fcce09f9e96524962e38f9ebdcb95
jvgranados <jgranadosflores@gmail.com>
2020-08-24 13:27:55 +0200

    Testing with shallow rendering

---
+++ frontend/__tests__/item.test.js
@@ -0,0 +1,31 @@
+import { shallow } from 'enzyme';
+import ItemComponent from '../components/Item';
+
+const fakeItem = {
+  id: 'ABC123',
+  title: 'A Cool Item',
+  price: 4000,
+  description: 'This item is really cool!',
+  image: 'dog.jpg',
+  largeImage: 'largedog.jpg',
+};
+
+describe('<Item />', () => {
+  const wrapper = shallow(<ItemComponent item={fakeItem} />);
+  it('renders the image properly', () => {
+    const img = wrapper.find('img');
+    expect(img.props().src).toBe(fakeItem.image);
+    expect(img.props().alt).toBe(fakeItem.title);
+  });
+  it('renders the pricetag and title', () => {
+    const PriceTag = wrapper.find('Item__PriceTag');
+    expect(PriceTag.children().text()).toBe('$40');
+    expect(wrapper.find('Item__Title a').text()).toBe(fakeItem.title);
+  });
+  it('renders out the buttons properly', () => {
+    const buttonList = wrapper.find('.buttonList');
+    expect(buttonList.children()).toHaveLength(3);
+    expect(buttonList.find('Link')).toHaveLength(1);
+    expect(buttonList.find('AddToCart').exists()).toBe(true);
+  });
+});
